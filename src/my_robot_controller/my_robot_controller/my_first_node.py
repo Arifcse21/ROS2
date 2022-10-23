74a250a9b5f8e6af1d85181ac76bc58d7b61f4ae
Arifcse21 <arifcse21@gmail.com>
2022-10-23 21:17:19 +0600

    first_node created

---
+++ src/my_robot_controller/my_robot_controller/my_first_node.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import rclpy
+from rclpy.node import Node
+
+
+class MyNode(Node):
+    def __init__(self):
+        super(MyNode, self).__init__("first_node")
+        self.create_timer(1.0, self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello Sweetie!")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MyNode()
+
+    rclpy.spin(node)
+
+
+    
+
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
