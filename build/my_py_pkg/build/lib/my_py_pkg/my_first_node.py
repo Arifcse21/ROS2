7a6fe2d74d273d754f15d8c88c1f4bcff964bcee
Arifcse21 <arifcse21@gmail.com>
2022-12-10 21:24:29 +0600

    class node oop

--- build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
+++ build/my_py_pkg/build/lib/my_py_pkg/my_first_node.py
@@ -3,10 +3,21 @@ import rclpy
 from rclpy.node import Node
 
 
+class MyNode(Node):
+    def __init__(self):
+        super(MyNode, self).__init__("py_test")
+        self.counter = 0
+        self.get_logger().info("Starting ROS2 Node!...")
+        self.create_timer(1, self.timer_callback)
+
+    def timer_callback(self):
+        self.counter += 1
+        self.get_logger().info("Hello BroBot! " + str(self.counter))
+
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")  # don't use 'node' as tail. it would be redundant
-    node.get_logger().info("Running ROS2 Node!...")
+    node = MyNode()
     rclpy.spin(node)
 
     rclpy.shutdown()
